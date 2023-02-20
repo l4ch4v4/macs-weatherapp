f131f8ca16e75ae954756a64ffaec9fdf96470b5
l4ch4v4 <124009201+l4ch4v4@users.noreply.github.com>
2023-02-20 01:02:49 -0700

    I finished adding the API in the app.js

--- weatherapp-stuff/src/app.js
+++ weatherapp-stuff/src/app.js
@@ -1,3 +1,9 @@
 let apiKey = "79f47b6b9113e21a5b5e8344934cbfbe";
 let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=Denver&appid=79f47b6b9113e21a5b5e8344934cbfbe";
+  "https://api.openweathermap.org/data/2.5/weather?q=Denver,Co&units=metric";
+
+function showTempurature(response) {
+  console.log(response.data);
+}
+
+axios.get(`${apiUrl}&appid=${apiKey}`).then(showTempurature);
