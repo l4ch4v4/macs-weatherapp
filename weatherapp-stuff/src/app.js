732fe9378121f25bc724e7f44a37958a54849ab7
l4ch4v4 <124009201+l4ch4v4@users.noreply.github.com>
2023-02-20 04:06:45 -0700

    I updated the app.js by  moving around the apiKey and apiUrl

--- weatherapp-stuff/src/app.js
+++ weatherapp-stuff/src/app.js
@@ -1,6 +1,20 @@
-let apiKey = "79f47b6b9113e21a5b5e8344934cbfbe";
-let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=Denver&units=metric";
+function formaDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednsday",
+    "Thursday",
+    "Thursday",
+    "Friday",
+    "Satureday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 
 function showTemperature(response) {
   console.log(response.data);
@@ -8,10 +22,18 @@ function showTemperature(response) {
   let cityElement = document.querryselector("#city");
   let description = document.querySelector("#temperature-description");
   let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   let temperature = Math.round(response.data.main.temp);
   temperatureElement.innerHTML = `${temperature}℃`;
+  cityElement.innerHTML = response.data.name;
   description.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formaDate(response.data.dt * 1000);
 }
+
+let apiKey = "79f47b6b9113e21a5b5e8344934cbfbe";
+let apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q=Denver&units=metric";
 
 axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
