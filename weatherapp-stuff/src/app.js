74546e2a9e26ebf6bbee2de83d790f104ec0157e
l4ch4v4 <124009201+l4ch4v4@users.noreply.github.com>
2023-02-20 01:40:46 -0700

    updating the app.js by adding showTemp function

--- weatherapp-stuff/src/app.js
+++ weatherapp-stuff/src/app.js
@@ -1,9 +1,14 @@
 let apiKey = "79f47b6b9113e21a5b5e8344934cbfbe";
 let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=Denver,Co&units=metric";
+  "https://api.openweathermap.org/data/2.5/weather?q=Denver&units=metric";
 
-function showTempurature(response) {
+function showTemperature(response) {
   console.log(response.data);
+  let temperature = Math.round(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  let description = document.querySelector("#temperature-description");
+  temperatureElement.innerHTML = `${temperature}℃`;
+  description.innerHTML = response.data.weather[0].description;
 }
 
-axios.get(`${apiUrl}&appid=${apiKey}`).then(showTempurature);
+axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
