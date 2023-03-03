52738410947ac9f3abe8924874b8c245aae2e616
l4ch4v4 <124009201+l4ch4v4@users.noreply.github.com>
2023-03-03 11:41:37 -0700

    I have updated the JS by having the forecast Element equal  forecastElement.innerHTML

--- weatherapp-stuff/src/app.js
+++ weatherapp-stuff/src/app.js
@@ -25,23 +25,25 @@ function formatDate(timestamp) {
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
 
-  forecastElement.innerHTML = `
-  <div class="weather-forcast" id="forcast">
-                    <div class="row">
-                        <div class="col-2">
-                            <div class="weather-forecast">Thur</div>
-                            <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="" id="icon"
-                                width="36" />
-                            <div class="weather-forcast-temp">
-                                <span class="weather-forecast-temperature-max">
-                                    10°</span>
-                                <span class="weather-forecast-temperature-min">
-                                    5°
-                                </span>
+  let forecastHTML = "";
+  forecastHTML = `
+    <div class="row">
+      <div class="col-2">
+        <div class="weather-forecast">Thur</div>
+        <img
+          src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+          alt=""
+          id="icon"
+          width="36"
+        />
+        <div class="weather-forcast-temp">
+          <span class="weather-forecast-temperature-max">10°</span>
+          <span class="weather-forecast-temperature-min">5°</span>
+        </div>
+      </div>
+    </div>`;
 
-                            </div>
-                        </div>
-                    </div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function showTemperature(response) {
