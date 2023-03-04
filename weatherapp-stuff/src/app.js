33413d769dc788a2f65ee72135f8c86f49b395d4
l4ch4v4 <124009201+l4ch4v4@users.noreply.github.com>
2023-03-04 15:27:41 -0700

    I got rid of the Celsius data, and display call, as well on the CSS and HTL

--- weatherapp-stuff/src/app.js
+++ weatherapp-stuff/src/app.js
@@ -123,27 +123,7 @@ function displayFahrenheitTemperature(event) {
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-
-function celsiusMetric(event) {
-  event.preventDefault;
-}
-
-let celsiusTemperature = null;
-
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Denver");
