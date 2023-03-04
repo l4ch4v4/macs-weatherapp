721d8c4c9a8461a019c84efea677d1f553601111
l4ch4v4 <124009201+l4ch4v4@users.noreply.github.com>
2023-03-04 14:18:47 -0700

    I have stored the response from the API inside the forecast  then I checked out all 8  variables and made and index for them. I was struggling with the icon image for the forecast, so I updated that link. Afterwards I rounded both max and min forecast- temperature

--- weatherapp-stuff/src/app.js
+++ weatherapp-stuff/src/app.js
@@ -21,31 +21,46 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Sat", "Sun", "Mon"];
-
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
       <div class="col-2">
-        <div class="weather-forecast">${day}</div>
+        <div class="weather-forecast">${formatDay(forecastDay.dt)}</div>
         <img
-          src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
           alt=""
           id="icon"
           width="36"
         />
         <div class="weather-forcast-temp">
-          <span class="weather-forecast-temperature-max">10°</span>
-          <span class="weather-forecast-temperature-min">5°</span>
+          <span class="weather-forecast-temperature-max">${Math.round(
+            forecastDay.temp.max
+          )}°</span>
+          <span class="weather-forecast-temperature-min">${Math.round(
+            forecastDay.temp.min
+          )}°</span>
         </div>
       </div>
       `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
