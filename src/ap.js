173f2dc9160476f2f3db6b6bb73ac6c198144316
l4ch4v4 <124009201+L4ch4v4@users.noreply.github.com>
2023-03-08 16:00:10 -0700

    I updated the APIkey, think I will do again

--- src/ap.js
+++ src/ap.js
@@ -68,7 +68,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "e0a5a97de9a0b7a951e9d154a8f9bad8";
+  let apiKey = "8cac06f7ab6c10287cd06a316ff84a57";
   console.log(coordinates);
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
