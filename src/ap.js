f216060acda504d7235216b64df9c473598fb05b
l4ch4v4 <124009201+L4ch4v4@users.noreply.github.com>
2023-03-11 15:49:47 -0700

    I have updated the temperature  display function

--- src/ap.js
+++ src/ap.js
@@ -87,10 +87,7 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.time * 1000);
-  iconElement.setAttribute(
-    "src",
-    iconElement.setAttribute("src", response.data.condition.icon_url)
-  );
+  iconElement.setAttribute("src", response.data.condition.icon_url);
   iconElement.setAttribute("alt", response.data.condition.description);
 
   getForecast(response.data.coordinates);
